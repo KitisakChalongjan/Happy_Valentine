17d948bb32efc1109b772bbc6417f53e93b09611
Dew Kitisak <Dewlonger15@gmail.com>
2025-02-14 21:25:19 +0700

    add /what_is_it endpoint

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
+
 	fmt.Println("Happy Valentine Service Started!")
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "🦉")
+	})
+
+	e.GET("/what_is_it", func(c echo.Context) error {
+		heart := `
+          ******       ******
+       **      **   **      **
+     **          ** **          **
+    **            ***            **
+    **             *              **
+     **                          **
+       **                    **
+         **                **
+           **            **
+             **        **
+               **    **
+                 **
+		`
+		return c.String(http.StatusOK, heart)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
